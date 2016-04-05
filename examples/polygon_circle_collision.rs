f9900e5531e901920952bf4c72f6a50b5a17f5d5
David Tibbetts <David Tibbetts>
2016-04-04 21:51:06 -0500

    Updating demos for use w/ latest SFML for Rust API

--- examples/polygon_circle_collision.rs
+++ examples/polygon_circle_collision.rs
@@ -2,9 +2,11 @@ extern crate sfml;
 extern crate box2d;
 extern crate time;
 
+// Latest version (1.11) of SFML for Rust requires a lot of traits to be included,
+//   so just use the entirety of graphics
+use sfml::graphics::*;
 use sfml::system::Vector2f;
-use sfml::window::{ContextSettings, VideoMode, event, Close};
-use sfml::graphics::{RenderWindow, RenderTarget, CircleShape, Color, PrimitiveType, Vertex};
+use sfml::window::{Key, VideoMode, event, window_style};
 
 use box2d::world::World;
 use box2d::body::BodyDef;
@@ -14,6 +16,12 @@ use box2d::math::Vec2;
 use time::Duration;
 use time::get_time;
 
+// Box2D tolerances are tuned to work in meters-kilogram-second (MKS) units.
+// Using floating point numbers, shapes work best between 0.1 and 10 meters.
+// It is HIGHLY recommended to have a meter-to-pixel scale to stay within these bounds.
+// Here we will use 1 meter == 100 pixels.
+const meters_to_pixels: f32 = 100.0;
+
 fn main() {
     let step = 1.0 / 60.0;
     let mut current_time: f64 = 0.0;
@@ -22,28 +30,39 @@ fn main() {
     // Create the window of the application
     let mut window = RenderWindow::new(VideoMode::new_init(800, 600, 32),
                                        "Polygon Circle Collision",
-                                       Close,
-                                       &ContextSettings::default())
+                                       window_style::CLOSE,
+                                       &Default::default())
                          .expect("Cannot create a new Render Window.");
+    // No tearing
+    window.set_vertical_sync_enabled(true);
 
     let mut world = setup_box2d();
+    let mut paused = true;
 
     while window.is_open() {
         // Handle events
         for event in window.events() {
             match event {
                 event::Closed => window.close(),
+                event::KeyPressed { code, .. } => {
+                    match code {
+                        Key::Escape => return,
+                        Key::Space => paused = !paused,
+                        _ => {}
+                    }
+                },
                 _             => {/* do nothing */}
             }
         }
 
         let time_data = get_time();
         let new_time = (Duration::seconds(time_data.sec as i64) + Duration::nanoseconds(time_data.nsec as i64)).num_milliseconds() as f64 / 1000.0;
-        let frame_time = (new_time - current_time).min(0.2);
+        // Don't count frame_time when paused
+        let frame_time = if !paused { (new_time - current_time).min(0.2) } else { 0.0 };
         current_time = new_time;
 
         accumulator = accumulator + frame_time;
-        while accumulator >= step {
+        while accumulator >= step && !paused {
             world.step(step as f32);
             accumulator -= step;
         }
@@ -55,39 +74,73 @@ fn main() {
             match *shape {
                 box2d::shape::shape::Shape::CircleShape{center, radius} => {
                     let mut circle = CircleShape::new().expect("Error, cannot create ball.");
-                    let position = world.bodies[i].position + center;
+                    // Units in Box2D should be converted from Meters to Pixels
+                    let position = (world.bodies[i].position + center).multiply(meters_to_pixels);
+                    let radius = radius * meters_to_pixels;
                     circle.set_radius(radius-1.0);
                     circle.set_outline_thickness(1.0);
-                    circle.set_outline_color(&Color{red: 255, green: 0, blue: 0, alpha: 255});
+                    circle.set_outline_color(&Color::new_rgb(255, 0, 0));
                     circle.set_fill_color(&Color::transparent());
                     circle.set_position(&Vector2f::new(position.x, position.y));
                     circle.set_origin(&Vector2f::new(radius, radius));
                     window.draw(&circle);
                 },
+
                 box2d::shape::shape::Shape::LineShape{point1, point2} => {
-                    let point1_global = world.bodies[i].position + point1;
-                    let point2_global = world.bodies[i].position + point2;
-                    let points = [Vertex::new_with_pos_color(&Vector2f::new(point1_global.x, point1_global.y), &Color::blue()),
-                    Vertex::new_with_pos_color(&Vector2f::new(point2_global.x, point2_global.y), &Color::blue())];
-                    window.draw_primitives(&points, PrimitiveType::Lines);
+                    // Units in Box2D should be converted from Meters to Pixels
+                    let point1_global = (world.bodies[i].position + point1).multiply(meters_to_pixels);
+                    let point2_global = (world.bodies[i].position + point2).multiply(meters_to_pixels);
+                    
+                    // Latest SFML uses new type, VertexArray, to draw primitive types
+                    let mut points = VertexArray::new().unwrap();
+                    points.set_primitive_type(Lines);
+                    points.append(&Vertex::new_with_pos_color(&Vector2f {
+                                                                    x: point1_global.x,
+                                                                    y: point1_global.y
+                                                                },
+                                                                &Color::blue()));
+                    points.append(&Vertex::new_with_pos_color(&Vector2f {
+                                                                    x: point2_global.x,
+                                                                    y: point2_global.y
+                                                                }, &Color::blue()));
+                    window.draw(&points);
                 },
                 box2d::shape::shape::Shape::ChainLineShape{ref points} => {
-                    let mut global_points: Vec<Vertex> = Vec::new();
+                    // Latest SFML uses new type, VertexArray, to draw primitive types
+                    let mut global_points = VertexArray::new().unwrap();
+                    global_points.set_primitive_type(LinesStrip);
                     for p in points.iter() {
-                        let global_point = world.bodies[i].position + *p;
-                        global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::blue()));
+                        // Units in Box2D should be converted from Meters to Pixels
+                        let mut global_point = (world.bodies[i].position + *p).multiply(meters_to_pixels);
+                        global_points.append(&Vertex::new_with_pos_color(&Vector2f {
+                                                                                x: global_point.x,
+                                                                                y: global_point.y
+                                                                            },
+                                                                            &Color::blue()));
                     }
-                    window.draw_primitives(&global_points, PrimitiveType::LinesStrip);
+                    window.draw(&global_points);
                 },
                 box2d::shape::shape::Shape::PolygonShape{ref points} => {
-                    let mut global_points: Vec<Vertex> = Vec::new();
+                    // Latest SFML uses new type, VertexArray, to draw primitive types
+                    let mut global_points = VertexArray::new().unwrap();
+                    global_points.set_primitive_type(LinesStrip);
                     for p in points.iter() {
-                        let global_point = world.bodies[i].position + *p;
-                        global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
+                        // Units in Box2D should be converted from Meters to Pixels
+                        let global_point = (world.bodies[i].position + *p).multiply(meters_to_pixels);
+                        global_points.append(&Vertex::new_with_pos_color(&Vector2f {
+                                                                            x: global_point.x,
+                                                                            y: global_point.y
+                                                                        },
+                                                                        &Color::red()));
                     }
-                    let global_point = world.bodies[i].position + points[0];
-                    global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
-                    window.draw_primitives(&global_points, PrimitiveType::LinesStrip);
+                    // Close off polygon by adding first point to end
+                    let global_point = (world.bodies[i].position + points[0]).multiply(meters_to_pixels);
+                    global_points.append(&Vertex::new_with_pos_color(&Vector2f {
+                                                                            x: global_point.x,
+                                                                            y: global_point.y
+                                                                        },
+                                                                        &Color::red()));
+                    window.draw(&global_points);
                 }
             }
         }
@@ -96,14 +149,42 @@ fn main() {
 }
 
 fn setup_box2d() -> World {
-    let mut world = World::new(Vec2::new(0.0, 5.0));
+    // Safe to use the box2d shapes here, be aware, though, SFML has similar shape names.
+    use box2d::shape::shape::Shape::*;
 
-    let polygon_shape = box2d::shape::shape::Shape::PolygonShape{ points: vec![Vec2::new(-25.0, -50.0), Vec2::new(-75.0, 75.0), Vec2::new(75.0, 75.0), Vec2::new(75.0, -75.0)] };
-    let polygon_body_def = BodyDef{shape: polygon_shape, body_type: BodyType::StaticBody, position: Vec2::new(400.0, 400.0), velocity: Vec2::new(0.0, 0.0), restitution: 1.0, mass: 0.0, gravity_scale: 1.0};
+    // Earth gravity, 9.8m/s
+    let mut world = World::new(Vec2::new(0.0, 9.8));
+
+    let polygon_shape = PolygonShape {
+                            points: vec! [
+                                Vec2::new(-0.25, -0.50),
+                                Vec2::new(-0.75, 0.75),
+                                Vec2::new(0.75, 0.75),
+                                Vec2::new(0.75, -0.75)
+                        ]};
+    let polygon_body_def = BodyDef {
+                            shape: polygon_shape,
+                            body_type: BodyType::StaticBody,
+                            position: Vec2::new(4.0, 4.0),
+                            velocity: Vec2::new(0.0, 0.0),
+                            restitution: 1.0,
+                            mass: 0.0,
+                            gravity_scale: 1.0
+                        };
     world.add_body(polygon_body_def);
 
-    let circle_shape = box2d::shape::shape::Shape::CircleShape{ center: Vec2::new(0.0, 0.0), radius: 20.0 };
-    let circle_body_def = BodyDef{shape: circle_shape, body_type: BodyType::DynamicBody, position: Vec2::new(350.0, 100.0), velocity: Vec2::new(0.0, 0.0), restitution: 1.0, mass: 10.0, gravity_scale: 10.0};
+    let circle_shape = CircleShape {
+                            center: Vec2::new(0.0, 0.0),
+                            radius: 0.20
+                        };
+    let circle_body_def = BodyDef {
+                            shape: circle_shape,
+                            body_type: BodyType::DynamicBody,
+                            position: Vec2::new(3.50, 1.0),
+                            velocity: Vec2::new(0.0, 0.0),
+                            restitution: 1.0,
+                            mass: 10.0,
+                            gravity_scale: 10.0};
     world.add_body(circle_body_def);
 
     return world;
